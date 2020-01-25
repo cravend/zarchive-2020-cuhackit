e5b6123054b7dd403c158a2100cd979b4ed85fc4
Joshua Nelson <jyn514@gmail.com>
2020-01-25 14:16:49 -0500

    add database schema

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+CREATE TEMPORARY TABLE medicines (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL
+);
+
+INSERT INTO medicines (name) VALUES ('dummy medicine');
+
+CREATE TEMPORARY TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+-- many to many relationship: user <=> medicine
+CREATE TEMPORARY TABLE schedules (
+  id SERIAL PRIMARY KEY,
+  medicine INT NOT NULL REFERENCES medicines(id),
+  user_id INT NOT NULL REFERENCES users(id),
+  weekdays VARCHAR(7) NOT NULL
+);
+
+CREATE TEMPORARY TABLE taken (
+  id SERIAL PRIMARY KEY,
+  medicine INT NOT NULL REFERENCES medicines(id),
+  user_id INT NOT NULL REFERENCES users(id),
+  datetime TIMESTAMP NOT NULL
+);
+
+SELECT * FROM medicines;
