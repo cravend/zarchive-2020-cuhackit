21e1725532a83ba850479aea068dacd663cb63f8
Dalton Craven <20dalton00@gmail.com>
2020-01-25 22:18:35 -0500

    add index module

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,11 +1,12 @@
 import Container from '../components/Container'
 import Day from '../components/Day'
 import Register from '../components/Register'
-import styles from '../theme/styles.scss'
+import styles from './index.module.scss'
 
 function HomePage() {
   return (
     <Container>
+      <h1 className={styles.title}>Welcome, Dalton!</h1>
       <div className={styles.flexContainer}>
         <Day name="Today" medicine={{ name: 'Atorvastatin', time: '22:00' }} />
         <Day name="Tomorrow" medicine={{ name: 'Tretonin', time: '07:00' }} />
