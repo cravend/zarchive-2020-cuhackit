27a352633374ff646cc8cc511ce840c94ba87639
Joshua Nelson <jyn514@gmail.com>
2020-01-26 03:25:56 -0500

    maybe fix CORS issues??

--- api/server.py
+++ api/server.py
@@ -1,8 +1,11 @@
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import api
 
 app = Flask(__name__)
+CORS(app, resources={"/*": {"origins": "localhost"}})
+
 api.init()
 
 @app.route("/add-medicine", methods=['POST'])
