b53576fc9aacdcf6139457f7f5c4f6ee066c3391
Joshua Nelson <jyn514@gmail.com>
2020-01-26 03:28:57 -0500

    make username optional

--- api/server.py
+++ api/server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import api
 
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": "localhost"}})
+CORS(app)
 
 api.init()
 
@@ -16,24 +16,24 @@ def add_medicine_handler():
 @app.route("/add-schedule", methods=['POST'])
 def add_schedule_handler():
     data = request.json
-    user, medicine, days, time = data['user'], data['medicine'], data['days'], data['time']
+    user, medicine, days, time = data.get('user', 1), data['medicine'], data['days'], data['time']
     id = api.add_schedule(user, medicine, days, time)
     return jsonify({ 'id': id })
 
 @app.route("/add-taken", methods=['POST'])
 def add_taken_handler():
     data = request.json
-    user, medicine, time = data['user'], data['medicine'], data['time']
+    user, medicine, time = data.get('user', 1), data['medicine'], data['time']
     id = api.add_taken(user, medicine, time)
     return jsonify({ 'id': id })
 
 @app.route("/username", methods=['GET'])
 def username_handler():
-    return jsonify({ 'name': api.username(0) })
+    return jsonify({ 'name': api.username(data.get('user', 1)) })
 
 @app.route("/notifications", methods=['GET'])
 def notification_handler():
     data = request.args
-    user, day = data['user'], data['day']
+    user, day = data.get('user', 1), data['day']
     notifications = api.notifications_for_week(user, day)
     return jsonify(notifications)
